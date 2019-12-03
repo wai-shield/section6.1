b58c15cf0b708a2453c8f6e0ca50dbaf8d20f09a
wai-shield <mwatch999@gmail.com>
2019-12-03 10:50:23 +1000

    Tutorial done 38

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -7,23 +7,57 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          /*child: Container(
+            height: 100.0,
+            width: 100.0,
+            //margin: EdgeInsets.only(left: 30.0),
+            //padding: EdgeInsets.all(20.0),
+            color: Colors.white,
+            child: Text("Container 1"),
+          ),*/
+          child : Row(
+            //mainAxisSize: MainAxisSize.min, //as per child widget
+            //verticalDirection: VerticalDirection.up, //1,2,3 or 3,2,1
+            //mainAxisAlignment: MainAxisAlignment.start, //place at top(start), botton(end), center (if not min in MainAxisSize)
+            //mainAxisAlignment: MainAxisAlignment.spaceBetween, //stretch (spaceEvenly, spaceBetween
+            crossAxisAlignment: CrossAxisAlignment.stretch, //like above in horizontel way
+
+            children: <Widget>[
+              Container(
+                //height: 100.0,
+                //width: 100.0,
+                color: Colors.white,
+                child: Text("Container 1"),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                //height: 100.0,
+                //width: 100.0,
+                color: Colors.lightBlue,
+                child: Text("Container 2"),
+              ),
+              Container(
+                //height: 100.0,
+                //width: 100.0,
+                color: Colors.red,
+                child: Text("Container 3"),
+              ),
+
+
+            ]
+          )
+        ),
+      )
     );
   }
 }
+
+
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
