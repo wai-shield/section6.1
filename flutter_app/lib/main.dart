e04dda3aad75e79cdf9948cc45f84f19ba0cb56a
wai-shield <mwatch999@gmail.com>
2019-12-03 12:49:48 +1000

    Card is used in all

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -72,23 +72,25 @@ class MyApp extends StatelessWidget {
 
 
                     Card(
-                      color: Colors.white,
+                      //color: Colors.white,
                       margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
                       //padding: EdgeInsets.all(10.0),
 
                       child: Padding(
                         padding: EdgeInsets.all(24.0),
-                        child: Row(
-                          children: <Widget>[
 
-                            Icon(Icons.phone,color:Colors.teal),
+                        child: ListTile(
 
-                            SizedBox(
+                          //children: <Widget>[
+
+                            leading: Icon(Icons.phone,color:Colors.teal),
+
+                            /*SizedBox(
                               width:10.0,
 
-                            ),
+                            ),*/
 
-                            Text("+ 096116905",
+                            title: Text("+ 096116905",
                                   style: TextStyle(
                                     color: Colors.teal.shade900,
                                     fontFamily: 'Cinzel',
@@ -97,18 +99,33 @@ class MyApp extends StatelessWidget {
                             )
 
 
-                          ],
+                       //   ],
                         ),
                       )
                     ),
 
 
 
-                    Container(
-                        color: Colors.white,
+                    Card(
+                        //color: Colors.white,
                         margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                        padding: EdgeInsets.all(10.0),
-                        child: Row(
+                        //padding: EdgeInsets.all(10.0),
+                        child: Padding(
+                          padding: EdgeInsets.all(24.0),
+                          child: ListTile(
+                            leading: Icon(Icons.email,color:Colors.teal),
+                            title:Text("moorejohn90@waisheldsoftware.com",
+                                style: TextStyle(
+                                    color: Colors.teal.shade900,
+                                    fontFamily: 'Cinzel',
+                                    fontSize: 20
+                                )
+                            )
+
+                          )
+
+                        )
+                        /*child: Row(
                           children: <Widget>[
 
                             Icon(Icons.email,color:Colors.teal),
@@ -128,7 +145,7 @@ class MyApp extends StatelessWidget {
 
 
                           ],
-                        )
+                        )*/
                     )
                   ],
                 )
