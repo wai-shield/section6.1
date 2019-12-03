28c2d1b13d5c9470e978823dc35817061ebd5024
wai-shield <mwatch999@gmail.com>
2019-12-03 12:38:07 +1000

    Using card widget

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -7,154 +7,135 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          /*child: Container(
-            height: 100.0,
-            width: 100.0,
-            //margin: EdgeInsets.only(left: 30.0),
-            //padding: EdgeInsets.all(20.0),
-            color: Colors.white,
-            child: Text("Container 1"),
-          ),*/
-          child : Row(
-            //mainAxisSize: MainAxisSize.min, //as per child widget
-            //verticalDirection: VerticalDirection.up, //1,2,3 or 3,2,1
-            //mainAxisAlignment: MainAxisAlignment.start, //place at top(start), botton(end), center (if not min in MainAxisSize)
-            mainAxisAlignment: MainAxisAlignment.spaceBetween, //stretch (spaceEvenly, spaceBetween
-            crossAxisAlignment: CrossAxisAlignment.stretch, //like above in horizontel way
+        home: Scaffold(
+            backgroundColor: Colors.teal,
+            body: SafeArea(
 
-            children: <Widget>[
-              Container(
-                //height: 100.0,
-                width: 100.0,
-                color: Colors.red,
-                child: Text("Container 1"),
-              ),
-              /*SizedBox(
-                height: 20.0,
-              ),*/
-              Container(
                 child: Column(
+                  //mainAxisSize: MainAxisSize.min, //as per child widget
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Container(
-                      width:100.0,
-                      height: 100.0,
-                      color: Colors.yellow
+
+
+                    CircleAvatar(
+                      radius: 50.0,
+                      backgroundColor: Colors.red,
                     ),
+
+
+                    Text(
+                      'Moore John',
+                       style: TextStyle(
+                         fontSize: 40.0,
+                         color: Colors.white,
+                         fontWeight: FontWeight.bold,
+                         fontFamily: 'IndieFlower',
+                       )
+                    ),
+
+
+                    Text(
+                      'Software Engineer',
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.teal[100],
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 2.6,
+                        fontFamily: 'Cinzel',
+
+                      )
+                    ),
+
+                    SizedBox(width:190, height: 20.0,child: Divider(color: Colors.teal.shade100)),
+
+                    Card(
+                        //color: Colors.white,
+                        margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                        //padding: EdgeInsets.all(10.0),
+
+                        child: Padding(
+                          padding: EdgeInsets.all(24.0),
+                          child: ListTile(
+                            leading: Icon(Icons.web,color:Colors.teal),
+                            title: Text("www.waishield.com",
+                                style: TextStyle(
+                                    color: Colors.teal.shade900,
+                                    fontFamily: 'Cinzel',
+                                    fontSize: 20
+                                )
+                            ),
+
+                          ),
+
+                        )
+                    ),
+
+
+                    Card(
+                      color: Colors.white,
+                      margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                      //padding: EdgeInsets.all(10.0),
+
+                      child: Padding(
+                        padding: EdgeInsets.all(24.0),
+                        child: Row(
+                          children: <Widget>[
+
+                            Icon(Icons.phone,color:Colors.teal),
+
+                            SizedBox(
+                              width:10.0,
+
+                            ),
+
+                            Text("+ 096116905",
+                                  style: TextStyle(
+                                    color: Colors.teal.shade900,
+                                    fontFamily: 'Cinzel',
+                                    fontSize: 20
+                                  )
+                            )
+
+
+                          ],
+                        ),
+                      )
+                    ),
+
+
+
                     Container(
-                      width: 100.0,
-                      height: 100.0,
-                      color: Colors.green,
+                        color: Colors.white,
+                        margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                        padding: EdgeInsets.all(10.0),
+                        child: Row(
+                          children: <Widget>[
+
+                            Icon(Icons.email,color:Colors.teal),
+
+                            SizedBox(
+                              width:10.0,
+
+                            ),
+
+                            Text("moorejohn90@waisheldsoftware.com",
+                                style: TextStyle(
+                                    color: Colors.teal.shade900,
+                                    fontFamily: 'Cinzel',
+                                    fontSize: 14
+                                )
+                            )
+
+
+                          ],
+                        )
                     )
                   ],
-                ),
-                /*height: 100.0,
-                width: 100.0,
-                color: Colors.white,
-                child: Text("Container 2"),*/
-              ),
-              Container(
-                //height: 100.0,
-                width: 100.0,
-                color: Colors.lightBlue,
-                child: Text("Container 3"),
-              ),
-
-
-            ]
-          )
-        ),
-      )
+                )
+            )
+        )
     );
   }
 }
 
 
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
